76c9f96f092f95219f2f42fb735418462ed5d634
God Johary <godjohary@gmail.com>
2023-03-13 21:26:00 +0300

    Update base.sql && Create data.sql

--- script/base.sql
+++ script/base.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS studio;
-CREATE DATABASE studio;
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
 
 CREATE TABLE genre (
     id_genre        SMALLINT NOT NULL PRIMARY KEY,
@@ -57,6 +57,7 @@ CREATE TABLE scene (
     ordre                       SMALLINT CHECK (ordre > 0),
     id_plateau                  INTEGER NOT NULL REFERENCES plateau (id_plateau),
     duration                    FLOAT NOT NULL CHECK (duration > 0),
+    estimation_tournage         TIME,
     debut_tournage_preferable   TIME,
     fin_tournage_preferable     TIME,
     debut_tournage              TIMESTAMP,
@@ -71,34 +72,3 @@ CREATE TABLE dialogue (
     dialogue            TEXT,
     "action"            TEXT
 );
-
-insert into type_plateau(id_type_plateau,libelle_type_plateau) values(1,'privee');
-insert into type_plateau(id_type_plateau, libelle_type_plateau) values(2,'public');
-
-insert into role_user(id_role,libelle_role) values(1,'scenariste');
-
-
-insert into genre(id_genre,libelle_genre) values(1,'homme');
-insert into genre(id_genre,libelle_genre) values(2,'femme');
-
-
-insert into users(username.password,id_role,id_genre,date_naissance) values('Jean','jean',1,1,'1999-10-12');
-
-insert into projet(titre,debut_production,fin_production) values('film1','2023-03-13','2023-03-30');
-
-insert into plateau(id_type_plateau,longitude,latitude,numero,lieu) values(1,45,32,1,'Anosy');
-insert into plateau(id_type_plateau,longitude,latitude,numero,lieu) values(2,30,31,2,'Analakely');
-insert into plateau(id_type_plateau,longitude,latitude,numero,lieu) values(2,24,32,3,'Andoharanofotsy');
-
-INSERT INTO scene (id_projet, id_scenariste, titre, ordre, id_plateau, duration, debut_tournage_preferable, fin_tournage_preferable)
-VALUES
-    (1, 1, 'Scène 1', 1, 1, 2.5, '08:00:00', '10:30:00'),
-    (1, 1, 'Scène 2', 2, 1, 1.5, '10:45:00', '12:15:00'),
-    (1, 1, 'Scène 3', 3, 2, 3.0, '13:00:00', '16:00:00'),
-    (1, 1, 'Scène 4', 4, 2, 2.0, '16:15:00', '18:15:00'),
-    (1, 1, 'Scène 5', 5, 3, 1.5, '09:00:00', '10:30:00'),
-    (1, 1, 'Scène 6', 6, 3, 2.5, '10:45:00', '13:15:00'),
-    (1, 1, 'Scène 7', 7, 1, 2.0, '13:00:00', '15:00:00'),
-    (1, 1, 'Scène 8', 8, 2, 1.0, '09:00:00', '10:00:00'),
-    (1, 1, 'Scène 9', 9, 3, 2.0, '13:30:00', '15:30:00'),
-    (1, 1, 'Scène 10', 10, 1, 1.5, '16:00:00', '17:30:00');
---
+++ script/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO type_plateau(id_type_plateau,libelle_type_plateau) VALUES (1,'privee');
+INSERT INTO type_plateau(id_type_plateau, libelle_type_plateau) VALUES (2,'public');
+
+INSERT INTO role_user(id_role,libelle_role) VALUES (1,'scenariste');
+
+
+INSERT INTO genre(id_genre,libelle_genre) VALUES (1,'homme');
+INSERT INTO genre(id_genre,libelle_genre) VALUES (2,'femme');
+
+
+INSERT INTO users(username.password,id_role,id_genre,date_naissance) VALUES ('Jean','jean',1,1,'1999-10-12');
+
+INSERT INTO projet(titre,debut_production,fin_production) VALUES ('film1','2023-03-13','2023-03-30');
+
+INSERT INTO plateau(id_type_plateau,longitude,latitude,numero,lieu) VALUES (1,45,32,1,'Anosy');
+INSERT INTO plateau(id_type_plateau,longitude,latitude,numero,lieu) VALUES (2,30,31,2,'Analakely');
+INSERT INTO plateau(id_type_plateau,longitude,latitude,numero,lieu) VALUES (2,24,32,3,'Andoharanofotsy');
+
+INSERT INTO scene (id_projet, id_scenariste, titre, ordre, id_plateau, duration, debut_tournage_preferable, fin_tournage_preferable)
+VALUES
+    (1, 1, 'Scène 1', 1, 1, 2.5, '08:00:00', '10:30:00'),
+    (1, 1, 'Scène 2', 2, 1, 1.5, '10:45:00', '12:15:00'),
+    (1, 1, 'Scène 3', 3, 2, 3.0, '13:00:00', '16:00:00'),
+    (1, 1, 'Scène 4', 4, 2, 2.0, '16:15:00', '18:15:00'),
+    (1, 1, 'Scène 5', 5, 3, 1.5, '09:00:00', '10:30:00'),
+    (1, 1, 'Scène 6', 6, 3, 2.5, '10:45:00', '13:15:00'),
+    (1, 1, 'Scène 7', 7, 1, 2.0, '13:00:00', '15:00:00'),
+    (1, 1, 'Scène 8', 8, 2, 1.0, '09:00:00', '10:00:00'),
+    (1, 1, 'Scène 9', 9, 3, 2.0, '13:30:00', '15:30:00'),
+    (1, 1, 'Scène 10', 10, 1, 1.5, '16:00:00', '17:30:00');
